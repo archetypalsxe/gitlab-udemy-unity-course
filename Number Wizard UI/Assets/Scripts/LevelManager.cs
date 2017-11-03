990cb3199fa25bd5bb0b95128bed73ba5588bfbd
Jack Ross, III <jross@partnercomm.net>
2017-11-03 08:54:27 -0400

    Almost done with Number Wizard UI

---
+++ Number Wizard UI/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	// Load up the requested level
+	public void LoadLevel(string name) {
+		Debug.Log("Level load requested: "+ name);
+		SceneManager.LoadScene(name);
+	}
+
+	// Quit the game
+	public void ExitGame() {
+		Debug.Log("Requested has been recevied to exit the game");
+		Application.Quit();
+	}
+}
---
+++ Number Wizard UI/Assets/Scripts/NumerWizard.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumerWizard : MonoBehaviour {
+
+	public Text guessText;
+
+	protected int max;
+	protected int min;
+	protected int numGuesses = 1;
+	protected int maxGuessesAllowed = 5;
+
+	public void GuessHigher(){
+		this.min = (this.min + this.max) / 2;
+		NextGuess();
+	}
+
+	public void GuessLower() {
+		this.max = (this.min + this.max) / 2;
+		NextGuess();
+	}
+
+	protected void updateGuessText() {
+		this.guessText.text = ((this.min + this.max) / 2).ToString();
+	}
+
+	protected void NextGuess() {
+		if(this.numGuesses >= this.maxGuessesAllowed) {
+			SceneManager.LoadScene("Win");
+		}
+		this.numGuesses++;
+		this.updateGuessText();
+	}
+
+	// Use this for initialization
+	void Start () {
+		this.max = 1000;
+		this.min = 1;
+		this.max++;
+		this.min--;
+		this.updateGuessText();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.UpArrow)) {
+			GuessHigher();
+		} else if(Input.GetKeyDown(KeyCode.DownArrow)) {
+			GuessLower();
+		}
+	}
+}
