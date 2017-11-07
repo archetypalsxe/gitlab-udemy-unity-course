0783255544d79b8da869270cd7738696a2bd5b4a
Jack Ross, III <jross@partnercomm.net>
2017-11-07 17:43:37 -0500

    Finished Section 4 (Number Wizard UI)

--- Number Wizard UI/Assets/Scripts/NumerWizard.cs
+++ Number Wizard UI/Assets/Scripts/NumerWizard.cs
@@ -8,39 +8,43 @@ public class NumerWizard : MonoBehaviour {
 
 	public Text guessText;
 
-	protected int max;
-	protected int min;
+	protected int max = 1000;
+	protected int min = 1;
+	protected int nextGuess;
 	protected int numGuesses = 1;
 	protected int maxGuessesAllowed = 5;
 
 	public void GuessHigher(){
-		this.min = (this.min + this.max) / 2;
+		this.min = this.nextGuess;
 		NextGuess();
 	}
 
 	public void GuessLower() {
-		this.max = (this.min + this.max) / 2;
+		this.max = this.nextGuess;
 		NextGuess();
 	}
 
+	protected void calculateNextGuess() {
+		this.nextGuess = Random.Range(this.min, this.max + 1);
+		//this.nextGuess = (this.min + this.max) / 2;
+	}
+
 	protected void updateGuessText() {
-		this.guessText.text = ((this.min + this.max) / 2).ToString();
+		this.guessText.text = this.nextGuess.ToString();
 	}
 
 	protected void NextGuess() {
 		if(this.numGuesses >= this.maxGuessesAllowed) {
 			SceneManager.LoadScene("Win");
 		}
+		calculateNextGuess();
 		this.numGuesses++;
 		this.updateGuessText();
 	}
 
 	// Use this for initialization
 	void Start () {
-		this.max = 1000;
-		this.min = 1;
-		this.max++;
-		this.min--;
+		this.calculateNextGuess();
 		this.updateGuessText();
 	}
 
