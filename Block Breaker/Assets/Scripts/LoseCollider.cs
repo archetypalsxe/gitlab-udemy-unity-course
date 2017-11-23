c04922eb37c4913efcfe11113032cd9fa667ee0f
Jack Ross, III <mrjackross@gmail.com>
2017-11-23 06:41:23 -0500

    More changes to Block Breaker. Apparently half way through?

---
+++ Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+
+	public void OnTriggerEnter2D (Collider2D trigger) {
+		this.levelManager.LoadLevel("Lose");
+	}
+}
