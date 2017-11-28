d1b23a9e4aa10509979fa94741746147fb4f29c2
Jack Ross, III <jross@partnercomm.net>
2017-11-28 07:37:23 -0500

    Progress on Block Breaker

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public Paddle paddle;
+
+	private bool hasStarted = false;
+	private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+		this.paddleToBallVector =
+			this.transform.position - this.paddle.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(!this.hasStarted) {
+			// Move the ball with the paddle
+			this.transform.position =
+				this.paddle.transform.position + this.paddleToBallVector;
+
+			if(Input.GetMouseButtonDown(0)) {
+				this.hasStarted = true;
+				this.GetComponent<Rigidbody2D>().velocity = new Vector2 (2f, 10f);
+			}
+		}
+	}
+}
---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 paddlePosition = new Vector3 (
+			Mathf.Clamp(
+				Input.mousePosition.x / Screen.width * 16,
+				0.5f,
+				15.5f
+			),
+			this.transform.position.y,
+			this.transform.position.x
+		);
+		this.transform.position = paddlePosition;
+	}
+}
