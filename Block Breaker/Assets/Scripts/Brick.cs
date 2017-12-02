5ed186973e3dbb28eb4a4f59b00283a0d8239975
Jack Ross, III <mrjackross@gmail.com>
2017-12-02 16:55:54 -0500

    Progress on Block Breaker

---
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+	public int maxHits = 1;
+
+	protected int timesHit = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void OnCollisionEnter2D (Collision2D trigger) {
+		this.timesHit++;
+	}
+}
