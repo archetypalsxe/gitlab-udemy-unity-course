fc8c08827034f026a92e98f7ad689900597d17f0
Jack Ross, III <mrjackross@gmail.com>
2018-01-19 08:25:35 -0500

    Progress on Block Breaker

--- Block Breaker/Assets/Scripts/Paddle.cs
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public bool autoPlay = false;
 
+	private Ball ball;
+
+	public void Start() {
+		this.ball = GameObject.FindObjectOfType<Ball>();
 	}
 
 	// Update is called once per frame
 	void Update () {
+		if(this.autoPlay) {
+			AutoMove();
+		} else {
+			MoveWithMouse();
+		}
+	}
+
+	void AutoMove() {
+		this.MovePaddle(ball.transform.position.x);
+	}
+
+	void MoveWithMouse() {
+		this.MovePaddle(Input.mousePosition.x / Screen.width * 16);
+	}
+
+	protected void MovePaddle(float xPosition) {
 		Vector3 paddlePosition = new Vector3 (
 			Mathf.Clamp(
-				Input.mousePosition.x / Screen.width * 16,
+				xPosition,
 				0.5f,
 				15.5f
 			),
